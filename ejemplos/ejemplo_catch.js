547f8e65a8ee70543a8b28dce553874c36cbc5cc
Fernando López <fernando.e.lopez@gmail.com>
2015-04-12 16:23:37 -0300

    Correcciones hasta el capítulo 4 inclusive más cuestiones de estilo

--- ejemplos/ejemplo_catch.js
+++ ejemplos/ejemplo_catch.js
@@ -1,4 +1,5 @@
-var server = new Server('ws://190.16.204.135:8000/api', '8d4ae089-443b-4bda-bd04-6601d664dd96');
+var server = new Server('ws://190.16.204.135:8000/api',
+                        '8d4ae089-443b-4bda-bd04-6601d664dd96');
 server.onConnect(function(){
     server.get_robots().then(function(robots){
         println(robots);
